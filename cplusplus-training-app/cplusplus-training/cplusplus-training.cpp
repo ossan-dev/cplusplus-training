81f369ddf4ae66fd00048641760778eb4f0d77a5
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-01-11 15:55:06 +0100

    modified main method to check if the compiler supports features of C++ 17 standard.

--- cplusplus-training-app/cplusplus-training/cplusplus-training.cpp
+++ cplusplus-training-app/cplusplus-training/cplusplus-training.cpp
@@ -1,11 +1,33 @@
 // cplusplus-training.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <array>
 #include <iostream>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+
+namespace a::b::c
+{
+    inline constexpr std::string_view str{ "Hello" };    
+}
+
+template <class... T>
+std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args) 
+{
+    return { sizeof...(T), (args + ...)};
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto [iNumbers, iSum] {sum(1, 2, 3)};
+    std::cout << a::b::c::str << ' ' << iNumbers << ' ' << iSum << '\n';
+
+    std::array arr{ 1, 2, 3 };
+
+    std::cout << std::size(arr) << '\n';
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
