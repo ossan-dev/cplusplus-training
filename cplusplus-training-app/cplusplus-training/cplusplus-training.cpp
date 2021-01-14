ac0f843eb9e1c43b72d54244abff186a775f3f7b
Ivan Pesenti <ivan.pesenti@abenergie.it>
2021-01-14 10:43:58 +0100

    restored the original hello world function

--- cplusplus-training-app/cplusplus-training/cplusplus-training.cpp
+++ cplusplus-training-app/cplusplus-training/cplusplus-training.cpp
@@ -1,32 +1,11 @@
 // cplusplus-training.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <array>
 #include <iostream>
-#include <string_view>
-#include <tuple>
-#include <type_traits>
-
-namespace a::b::c
-{
-    inline constexpr std::string_view str{ "Hello" };    
-}
-
-template <class... T>
-std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args) 
-{
-    return { sizeof...(T), (args + ...)};
-}
 
 int main()
 {
-    auto [iNumbers, iSum] {sum(1, 2, 3)};
-    std::cout << a::b::c::str << ' ' << iNumbers << ' ' << iSum << '\n';
-
-    std::array arr{ 1, 2, 3 };
-
-    std::cout << std::size(arr) << '\n';
-
+    std::cout << "Hello World!";
     return 0;
 }
 
